58ab31fa43ba5de7378b412235e8585a2514edda
caua <julianacaua@gmail.com>
2026-01-14 20:04:50 -0300

    second commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
+import { NgModule } from '@angular/core';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'categories', loadChildren: () => import('./pages/categories/categories.module').then(m => m.CategoriesRoutingModule) }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
