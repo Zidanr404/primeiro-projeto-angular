370919205947c5a231135cd9efe8d1ae9df2fa13
caua <julianacaua@gmail.com>
2026-01-14 18:36:15 -0300

    first commit

---
+++ src/app/app.ts
@@ -0,0 +1,20 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.scss'
+})
+export class App {
+  protected readonly title = signal('primeiro-projeto-angular');
+
+
+  funcao1 () {
+    alert("Função chamada!");
+  }
+
+    
+  
+}
