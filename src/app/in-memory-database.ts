09ca2872fe59d140a11163107dc48fae7a448a9f
caua <julianacaua@gmail.com>
2026-02-16 18:26:06 -0300

    adding in-memory-database for categories

---
+++ src/app/in-memory-database.ts
@@ -0,0 +1,14 @@
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+
+export class InMemoryDatabase implements InMemoryDbService {
+    createdDb() {
+        const categories = [
+           { id : 1, name: 'Lazer', description: 'Volei, cinema, praia, etc' }, 
+           { id : 2, name: 'Saúde', description: 'Exercícios, alimentação saudável, etc' },
+           { id : 3, name: 'Trabalho', description: 'Reuniões, prazos, etc'},
+           { id : 4, name: 'Família', description: 'Almoço de domingo, aniversário, etc' },
+           { id : 5, name: 'Amigos', description: 'Encontro, viagem, etc' },
+        ];
+        return { categories };
+    }
+}
